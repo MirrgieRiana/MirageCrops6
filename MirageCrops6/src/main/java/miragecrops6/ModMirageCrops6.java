90c905c95bfc01868ab422c8a2ccf0b665faed31
MirrgieRiana <devnull@localhost>
2015-08-06 22:31:19 +0900

    build.gradle: 自動的にソース中のバージョン番号を設定

--- MirageCrops6/src/main/java/miragecrops6/ModMirageCrops6.java
+++ MirageCrops6/src/main/java/miragecrops6/ModMirageCrops6.java
@@ -21,7 +21,7 @@ public class ModMirageCrops6
 
 	public static final String MODID = "MirageCrops6";
 	public static final String DOMAIN = MODID.toLowerCase();
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "${version}";
 
 	public static CreativeTabs creativeTab = new CreativeTabs("mirageCrops6") {
 		@Override
