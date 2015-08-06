e0a963db0854c2f512980ec29c3547bbd59bc652
MirrgieRiana <devnull@localhost>
2015-08-06 18:56:01 +0900

    HOreDictionary: Optionalクラスのimport修正

--- MirageCrops6/src/main/java/mirrg/minecraft/itemstack/mir60/HOreDictionary.java
+++ MirageCrops6/src/main/java/mirrg/minecraft/itemstack/mir60/HOreDictionary.java
@@ -1,12 +1,11 @@
 package mirrg.minecraft.itemstack.mir60;
 
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
-
-import com.google.common.base.Optional;
 
 public class HOreDictionary
 {
@@ -15,7 +14,7 @@ public class HOreDictionary
 	{
 		ArrayList<ItemStack> ores = OreDictionary.getOres(name);
 		if (ores.size() >= 1) return Optional.of(ores.get(0));
-		return Optional.absent();
+		return Optional.empty();
 	}
 
 	public static void findFirst(String name, Consumer<ItemStack> consumer)
